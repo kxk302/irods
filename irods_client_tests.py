51ae370dc35a8be6200e52df8d3c7ea227b1537c
kxk302 <kxk302@gmail.com>
2020-01-31 15:15:23 -0500

    Update irods_client_tests.py

--- irods_client_tests.py
+++ irods_client_tests.py
@@ -15,4 +15,4 @@ def test_get_collection():
         aCollection = "/" + zone + "/home/" + user
     
         coll = session.collections.get(aCollection)
-        assert coll.path ==! Got wrong collection path" aCollection, "Test failed
+        assert coll.path == aCollection, "Test failed! Got wrong collection path"
